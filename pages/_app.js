a24936439c2531d8c3994f3a1dc9481418c0e788
Shirley Wong <wongsh@oregonstate.edu>
2021-03-16 15:02:59 -0700

    Add navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import styled from '@emotion/styled';
+
+import Navbar from '../components/Navbar';
+
+const Content = styled.div`
+  margin-top: 50px;
+`
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Content>
+        <Component {...pageProps} />
+      </Content>
+    </>
+  );
 }
 
 export default MyApp
